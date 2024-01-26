82244d79cebd63e156ce916a9823f82ce481233a
Taha EBİŞ <tahaebis0@gmail.com>
2024-01-26 16:35:00 +0300

    Add project files.

---
+++ Controllers/AnnouncementsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErgunAkaryakitWebProject.Controllers
+{
+    public class AnnouncementsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErgunAkaryakitWebProject.Controllers
+{
+    public class StationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
